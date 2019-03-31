9ac6dc7249639e9d00052f752614ac83d55cbf7f
Andrew Snodgrass <git@pointlander.info>
2019-03-31 16:35:54 -0600

    Added focus group simulator

---
+++ src/focus_group.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import fire
+import json
+import os
+import numpy as np
+import tensorflow as tf
+import re
+import random
+
+import model, sample, encoder
+
+def interact_model(
+    model_name='117M',
+    seed=None,
+    nsamples=1,
+    batch_size=1,
+    length=64,
+    temperature=1,
+    top_k=40,
+):
+    """
+    Interactively run the model
+    :model_name=117M : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
+     results
+    :nsamples=1 : Number of samples to return total
+    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
+    """
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
+    enc = encoder.get_encoder(model_name)
+    hparams = model.default_hparams()
+    with open(os.path.join('models', model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx // 4
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k
+        )
+
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        saver.restore(sess, ckpt)
+        def sampleModel(raw_text):
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            for _ in range(nsamples // batch_size):
+                out = sess.run(output, feed_dict={
+                    context: [context_tokens for _ in range(batch_size)]
+                })[:, len(context_tokens):]
+                for i in range(batch_size):
+                    generated += 1
+                    text = enc.decode(out[i])
+                    position = re.search(r"[.!?]", text)
+                    if position is None:
+                        return text
+                    else:
+                        position = position.start() + 1
+                        return text[:position].strip()
+
+        bias = ["Donald Trump is great!", "Hillary Clinton is great!", "Bernie Sanders is great!"]
+        names = ["Trump", "Clinton", "Sanders"]
+        state = [[""] * 3 for i in range(3)]
+        index = [0] * 3
+        selected = random.randint(0,2)
+        while True:
+            ctx = bias[selected]
+            i = index[selected]
+            while True:
+                ctx += " " + state[selected][i]
+                i = (i + 1) % 3
+                if i == index[selected]:
+                    break
+            ctx = ctx.strip()
+            message = sampleModel(ctx)
+            print(names[selected] + ": " + message)
+            selected = random.randint(0, 2)
+            i = index[selected]
+            state[selected][i] = message
+            i = (i + 1) % 3
+            index[selected] = i
+
+if __name__ == '__main__':
+    fire.Fire(interact_model)
